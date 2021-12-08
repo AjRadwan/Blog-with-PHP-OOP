50d9861f54e683b5438acae1055efc3b3ceb38bf
Anowar <1hafizajr@gmail.com>
2021-12-08 12:42:33 +0600

    Initial setup

---
+++ about.php
@@ -0,0 +1,33 @@
+<?php include 'inc/header.php' ?>
+
+	<div class="contentsection contemplete clear">
+		<div class="maincontent clear">
+			<div class="about">
+				<h2>About us</h2>
+	
+				<p>About us..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
+				
+				<p>About me..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
+				
+				<p>About me..Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.</p>
+	</div>
+
+		</div>
+		<div class="sidebar clear">
+			<div class="samesidebar clear">
+				<h2>Latest articles</h2>
+					<ul>
+						<li><a href="#">Category One</a></li>
+						<li><a href="#">Category Two</a></li>
+						<li><a href="#">Category Three</a></li>
+						<li><a href="#">Category Four</a></li>
+						<li><a href="#">Category Five</a></li>						
+					</ul>
+			</div>
+			
+		
+			
+		</div>
+	 
+<?php include 'inc/sidebar.php' ?>
+<?php include 'inc/footer.php' ?>
