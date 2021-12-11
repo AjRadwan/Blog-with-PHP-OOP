0a5fb95196cb704003a79f84fabf3739622b057a
Anowar <1hafizajr@gmail.com>
2021-12-11 11:47:12 +0600

    added pagination

--- index.php
+++ index.php
@@ -13,8 +13,20 @@ $fm = new Format();
 
 <div class="contentsection contemplete clear">
 <div class="maincontent clear">
+	<!-- pagination -->
+<?php 
+$per_page = 3;
+if(isset($_GET['page'])){
+	$page = $_GET['page'];	
+}else{ 
+	$page = 1;
+}
+$start_form = ($page -1) * $per_page;
+?>
+	<!-- pagination -->
+
 <?php
-   $query = "SELECT * FROM tbl_post LIMIT 3";
+   $query = "SELECT * FROM tbl_post LIMIT $start_form, $per_page";
    $post = $db->select($query);
     if($post){
     while($result = $post->fetch_assoc()){
@@ -37,10 +49,18 @@ $fm = new Format();
  <?php }  ?> <!--end while loop -->
 
  <!-- pagination -->
- <?php echo "<span class='pagination'><a href='index.php?page=1'>".'First Page'."</a>" ?>
- 1, 2, 3
- <?php echo "<a href='index.php?page=1'>".'Last Page'."</a></span>" ?>
+ <?php 
+$query = "SELECT * FROM tbl_post";
+$result = $db->select($query);
+$total_rows = mysqli_num_rows($result);
+$total_pages = ceil($total_rows / $per_page);
 
+echo "<span class='pagination'><a href='index.php?page=1'>".'First Page'."</a>";
+ for($i = 1; $i <= $total_pages; $i++){
+	echo "<a href='index.php?page=".$i."'>".$i."</a>";
+}
+ echo "<a href='index.php?page=$total_pages'>".'Last Page'."</a></span>" ?>
+ <!-- pagination -->
 <?php } else { header("location:404.php"); } ?>
 </div>
 
