bec3b412a96442c649b27e70898d6c623f93a1b5
Anowar <1hafizajr@gmail.com>
2021-12-12 11:30:05 +0600

    add releted category

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,41 +1,32 @@
 <div class="sidebar clear">
-			<div class="samesidebar clear">
-				<h2>Categories</h2>
-					<ul>
-						<li><a href="#">Category One</a></li>
-						<li><a href="#">Category Two</a></li>
-						<li><a href="#">Category Three</a></li>
-						<li><a href="#">Category Four</a></li>
-						<li><a href="#">Category Five</a></li>						
-					</ul>
-			</div>
-			
-			<div class="samesidebar clear">
-				<h2>Latest articles</h2>
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-					
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-					
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-					
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-	
-			</div>
-			
+<div class="samesidebar clear">
+	<h2>Categories</h2>
+<?php
+ 
+$query = "SELECT * FROM tbl_category";
+   $category = $db->select($query);
+   if ($category) {
+	   while($result = $category->fetch_assoc()) {
+ 
+?>
+<ul>
+	<li><a href="posts.php?category=<?php echo $result['id'] ?>"><?php echo $result['name']; ?></a></li>
+
+	<?php } } else { ?>		
+		<li>NO Releted Categories</li>	
+	<?php } ?>		
+</ul>
+</div>
+
+<div class="samesidebar clear">
+	<h2>Latest articles</h2>
+		<div class="popular clear">
+			<h3><a href="#">Post title will be go here..</a></h3>
+			<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+			<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
 		</div>
+		
+	
+</div>
+
+</div>
