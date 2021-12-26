8a8a24bd593d851523a8aa2dea8a67150d393c63
Anowar <1hafizajr@gmail.com>
2021-12-26 11:28:39 +0600

    highlight curret page

--- inc/header.php
+++ inc/header.php
@@ -75,51 +75,59 @@ $(window).load(function() {
  $query = "SELECT * FROM title_slogan WHERE id = 1";
  $getData = $db->select($query);
  if ($getData) {
-	 while ($result = $getData->fetch_assoc()) {
+	while ($result = $getData->fetch_assoc()) {
 ?>		
-		<a href="#">
-			<div class="logo">
-				<img src="admin/<?php echo $result['logo'] ?>" alt="Logo"/>
-				<h2><?php echo $result['title'] ?></h2>
-				<p><?php echo $result['slogan'] ?></p>
-			</div>
-		</a>
+	<a href="#">
+		<div class="logo">
+			<img src="admin/<?php echo $result['logo'] ?>" alt="Logo"/>
+			<h2><?php echo $result['title'] ?></h2>
+			<p><?php echo $result['slogan'] ?></p>
+		</div>
+	</a>
 
 <?php } }?>
-		<div class="social clear">
-	<?php
- $query = "SELECT * FROM tbl_social WHERE id = 1";
- $getSocial = $db->select($query);
- if ($getSocial) {
-	 while ($data = $getSocial->fetch_assoc()) {
+	<div class="social clear">
+<?php
+$query = "SELECT * FROM tbl_social WHERE id = 1";
+$getSocial = $db->select($query);
+if ($getSocial) {
+	while ($data = $getSocial->fetch_assoc()) {
 ?>
 <div class="icon clear">
-	<a href="<?php echo $data['fb']?>" target="_blank"><i class="fa fa-facebook"></i></a>
-	<a href="<?php echo $data['tw']?>" target="_blank"><i class="fa fa-twitter"></i></a>
-	<a href="<?php echo $data['ln']?>" target="_blank"><i class="fa fa-linkedin"></i></a>
-	<a href="<?php echo $data['gp']?>" target="_blank"><i class="fa fa-google-plus"></i></a>
+<a href="<?php echo $data['fb']?>" target="_blank"><i class="fa fa-facebook"></i></a>
+<a href="<?php echo $data['tw']?>" target="_blank"><i class="fa fa-twitter"></i></a>
+<a href="<?php echo $data['ln']?>" target="_blank"><i class="fa fa-linkedin"></i></a>
+<a href="<?php echo $data['gp']?>" target="_blank"><i class="fa fa-google-plus"></i></a>
 </div>
 <?php } }?>
 
-			<div class="searchbtn clear">
-			<form action="search.php" method="get">
-				<input type="text" name="search" placeholder="Search keyword..."/>
-				<input type="submit" name="submit" value="Search"/>
-			</form>
-			</div>
+		<div class="searchbtn clear">
+		<form action="search.php" method="get">
+			<input type="text" name="search" placeholder="Search keyword..."/>
+			<input type="submit" name="submit" value="Search"/>
+		</form>
 		</div>
 	</div>
+</div>
 <div class="navsection templete">
-	<ul>
-		<li><a id="active" href="index.php">Home</a></li>
+<ul>
+	<li><a  href="index.php">Home</a></li>
+	<?php
+		$query = "SELECT * FROM tbl_page";
+		$pages = $db->select($query);
+		if ($pages) {
+		while ($result = $pages->fetch_assoc()){   ?>  
+		<li><a
 		<?php
-            $query = "SELECT * FROM tbl_page";
-            $pages = $db->select($query);
-            if ($pages) {
-            while ($result = $pages->fetch_assoc()){   ?>  
-            <li><a href="page.php?pageid=<?php echo $result['id']?>"><?php echo $result['name']?></a></li>
+//	highlight current page or menu item		 
+  if (isset($_GET['pageid']) && $_GET['pageid'] == $result['id']) {
+	 echo 'id="active"';
+  }
+		
+		?>
+		href="page.php?pageid=<?php echo $result['id']?>"><?php echo $result['name']?></a></li>
 
-           <?php }  } ?>
-		<li><a href="contact.php">Contact</a></li>
-	</ul>
+		<?php }  } ?>
+	<li><a href="contact.php">Contact</a></li>
+</ul>
 </div>
